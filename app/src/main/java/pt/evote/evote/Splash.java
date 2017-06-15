dc53d81c876795ed8a2063a08fc34e2c07579644
Ricardo Ribeiro <ricardoribeiro_1991@hotmail.com>
2017-06-15 23:01:12 +0100

    Add SplashScreen

---
+++ app/src/main/java/pt/evote/evote/Splash.java
@@ -0,0 +1,13 @@
+package pt.evote.evote;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
